27b7862ac7acf72cf3c28e668111fabd3818b973
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2021-03-17 14:43:33 +0100

    move import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
+import loadable from '@loadable/component';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import NavBar from './modules/navbar/navbar.connector';
 import UserRoutes from './modules/routes/userRoutes.connector';
-
 import Toaster from './modules/toaster/toast.connector';
-import loadable from '@loadable/component';
 
 const Admin = loadable(() => import('./pages/admin/admin.component'));
 
