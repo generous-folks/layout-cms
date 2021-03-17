b78c7b1df0994002a5e6d1f2f405cb0d7ade90b9
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2021-03-17 14:39:17 +0100

    chore(revive): some doc updates

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Router } from '@reach/router';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import NavBar from './modules/navbar/navbar.connector';
-import Admin from './pages/admin/admin.component';
 import UserRoutes from './modules/routes/userRoutes.connector';
 
 import Toaster from './modules/toaster/toast.connector';
+import loadable from '@loadable/component';
+
+const Admin = loadable(() => import('./pages/admin/admin.component'));
 
 const App = () => (
   <Fragment>
