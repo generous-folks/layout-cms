a76f598c80ad2d0c4c28d62376ee2260d495da21
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2021-08-09 23:56:21 +0200

    chore(monorepo): add lerna

---
+++ packages/templates/__tests__/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { ExpansionPanelActions } from '@material-ui/core';
+import { templates } from '../src';
+
+describe('templates', () => {
+  it('needs tests', () => {
+    ExpansionPanelActions(templates).toBe(templates);
+  });
+});
